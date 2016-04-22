1d027e2d3b6c3050c66809f63f9c80ff48a41bbc
Reneunodostres <carlosrenecbtis16@gmail.com>
2016-04-21 21:28:48 -0500

    intento3
    correcto

--- js/acciones.js
+++ js/acciones.js
@@ -3,15 +3,15 @@
 $(document).ready(function (e){
 	document .addEventListener("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
-		$('#disp table td').eq(5).text(device.Cordova);
+		$('#disp table td').eq(5).text(device.cordova);
 		$('#disp table td').eq(7).text(device.platform);
 		$('#disp table td').eq(9).text(device.version);
 		$('#disp table td').eq(11).text(device.uuid);
 		document.addEventListener("pause",function(){
-			escribehistoria ('la app se pauso');
+			escribehistoria ('la app se pausó');
 		},false);//pause
 		document.addEventListener("resume",function(){
-			escribehistoria ('la app se reinicio');
+			escribehistoria ('la app se reinició');
 		},false);//resume
 		document.addEventListener("online",function(){
 			escribehistoria ('la app se conecto a la red');
@@ -25,6 +25,6 @@ $(document).ready(function (e){
 });//document
 
 function escribehistoria(accion){
-	$('eHistoria').append('<li>'+accion+'</li>');
+	$('#eHistoria').append('<li>'+accion+'</li>');
 }
 		
